bc667af3ebb2e526680fc7026b55f7468fe46e27
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-06 09:32:51 -0500

    Tipos de datos operadores 1

--- 1.InicioVisualStudio/1.InicioVisualStudio/obj/Debug/net9.0/1.InicioVisualStudio.AssemblyInfo.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/obj/Debug/net9.0/1.InicioVisualStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3870df8fb2a4d56e947176094e1cb93913ebd180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9153c9ebc834d3c35fddbe8d659bda6752452ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 2.Operadores/2.Operadores/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _2.Operadores
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //OPERADORES
+            //Operadores numéricos
+            //Cambio de signo
+            int num1 = 5;
+            int num2 = -num1;
+            int num3 = +num2;
+            int num4 = -num3;
+
+            Console.WriteLine(num1 + "//" + num2 + "//" + num3 + "//" + num4);
+
+            //Aritméticas
+            int num5 = 3 + 5;//Suma
+            Console.WriteLine(num5);
+            int num6 = 100 - 26;//Resta
+            Console.WriteLine(num6);
+            int num7 = 4 * 2;//Producto
+            Console.WriteLine(num7);
+            int num8 = 5 / 3;//División
+            Console.WriteLine(num8);
+
+    }
+}
