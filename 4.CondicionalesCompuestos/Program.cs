fa0f56f82c5da931b83bb111d48553e94fcd2c43
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-15 11:58:43 -0500

    condicionales compuestos

--- 4.CondicionalesCompuestos/Program.cs
+++ 4.CondicionalesCompuestos/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /* 4.Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”.*/
+            /* 4.Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”.*/
 
 
 
