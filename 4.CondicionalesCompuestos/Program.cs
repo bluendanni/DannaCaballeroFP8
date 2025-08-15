fdc0c3e32e507f916595c9acfc556d7bf32e18c5
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-15 11:52:41 -0500

    If/true, if/false. Sueldo de una persona.

---
+++ 4.CondicionalesCompuestos/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _4.CondicionalesCompuestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 4.Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”.*/
+
+
+
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingresa tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu sueldo");
+            sueldo = Int32.Parse(Console.ReadLine());
+
+            if (sueldo >= 3000)
+            {
+                //Verdadera
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //Fake
+                Console.WriteLine($"La persona {nombre}, esta excento de impuestos");
+            }
+
+        }
+    }
+}
