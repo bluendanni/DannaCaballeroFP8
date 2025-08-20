bdf49c4b7aceded24beacb554d46d2156a1124a0
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-20 06:15:48 -0500

    Tarea 2. 2. Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”.

---
+++ Tarea2Algoritmos/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Tarea2Algoritmos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            Console.WriteLine("Ingresa tu nombre");
+            nombre = Console.ReadLine();
+
+
+            int edad = 0;
+            int numero1 = 0;
+            int numero2 = 0;
+
+            Console.WriteLine("Ingresa tu edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine($"Bienvenido a mi sitio web, {nombre}");
+            }
+            else
+            {
+                Console.WriteLine($"A mimir!");
+            }
+
+            Console.WriteLine("Ingresa Numero 1");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa Numero 2");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine(numero1 + numero2);
+                Console.WriteLine(numero1 - numero2);
+            }
+
+            else
+            {
+                Console.WriteLine(numero1 * numero2);
+                Console.WriteLine(numero1 / numero2);
+            }
+
+
+
+        }
+    }
+}
