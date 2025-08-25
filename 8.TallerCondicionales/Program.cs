5569513259a51a19bfd48dbd02119e7ed05d6bf8
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-25 15:48:44 -0500

    Ejercicio #1. El algoritmo debe comprobar si los números son diferentes, si es así, imprimir cual es el número mayor, además ordenar y mostrar en pantalla los números de menor a mayor.

--- 8.TallerCondicionales/Program.cs
+++ 8.TallerCondicionales/Program.cs
@@ -4,7 +4,7 @@ namespace _8.TallerCondicionales
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
            /*Crear un algoritmo que permita leer tres valores y almacenarlos en las variables numero1,numero2, numero3 respectivamente. El algoritmo debe comprobar si los números son diferentes, si es así, imprimir cual es el número mayor, además ordenar y mostrar en  pantalla los números de menor a mayor. Si hay números iguales, el algoritmo debe mostrar un mensaje indicando que se deben ingresar números diferentes.*/
 
