27cb6290c5315684cc89aadaf689ff2107a99915
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-25 15:47:46 -0500

    Ejercicio #2. El personaje de un juego puede disparar si cumple con las dos condiciones.

--- 4.CondicionalesCompuestos/Program.cs
+++ 4.CondicionalesCompuestos/Program.cs
@@ -6,8 +6,6 @@
         {
             /* 4.Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”.*/
 
-
-
             string nombre = "";
             int sueldo = 0;
 
---
+++ 8.Taller (ejercicio 2)/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _8.Taller__ejercicio_2_
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Ingresar si el personaje está en estado invencible
+            Console.WriteLine("¿El personaje está en estado invencible? (True/False): ");
+            bool invencible = Convert.ToBoolean(Console.ReadLine());
+
+            //Generar munición aleatoria con Random
+            Random rnd = new Random();
+            int municion = rnd.Next(0, 21); // Genera entre 0 y 20
+            Console.WriteLine($"Cantidad de munición: {municion}");
+
+            // Condición pedida
+            if (invencible && municion >= 1 && municion <= 10)
+            {
+                Console.WriteLine("El personaje está disparando");
+            }
+            //Si no cumple, no se hace nada
+        }
+    }
+}
