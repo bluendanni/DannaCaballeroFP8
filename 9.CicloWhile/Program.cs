209363186d14252eca45bbd76e1c60ed6713e26f
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-29 08:54:56 -0500

    5 numeros enteros

---
+++ 9.CicloWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador: {contador} - Acumulador:{acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine("La suma de los primeros 5 números enteros es:" + acumulador);
+
+        }   
+    }
+}
