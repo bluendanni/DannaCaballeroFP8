27b4d5a6599a341970526e4804af226c353fb372
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-29 09:12:43 -0500

    //10.Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1 * 2 * 3 * 4 * 5)

---
+++ 10.While2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //10.Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1 * 2 * 3 * 4 * 5)//
+            Console.WriteLine("Ingrese un número para calcular su factorial:");
+            int numero = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            int contador = 1;
+
+            while (contador <= numero)
+            {
+                factorial *= contador;
+                Console.WriteLine($"Contador: {contador} - Factorial:{factorial}");
+                contador++;
+            }
+            Console.WriteLine($"El factorial de {numero} es: {factorial}");
+
+        }
+    }
+}
