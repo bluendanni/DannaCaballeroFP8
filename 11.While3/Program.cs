a4642f75b24da4e08645650a4929fc661ac3e9e3
DannaCaballero <danna.caballero@upb.edu.co>
2025-08-29 09:27:15 -0500

    /*11. Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo.  Se debe mostrar por pantalla el total de la suma de los números ingresados. */

---
+++ 11.While3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _11.While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*11. Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo.
+             Se debe mostrar por pantalla el total de la suma de los números ingresados. */
+
+            int numero;
+            int suma = 0;
+
+            Console.WriteLine("Ingresa un número entero positivo (o un número negativo para finalizar): ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0)
+            {
+                suma += numero;
+                Console.WriteLine("Ingresa otro número entero positivo (o un número negativo para finalizar): ");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("La suma total de los números ingresados es: " + suma);
+
+        }
+    }
+}
