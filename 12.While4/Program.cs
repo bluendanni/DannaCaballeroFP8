4ecec8f997877509d2fe88ee6e6f672a93a17f58
DannaCaballero <danna.caballero@upb.edu.co>
2025-09-03 08:28:35 -0500

    //12.Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99.

---
+++ 12.While4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _12.While4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12.Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99. 
+            int contador = 1;
+            int suma = 0;
+
+            while (contador <= 99)
+            {
+                suma += contador;
+                Console.WriteLine($"La suma de los números impares hasta el 99 es: {suma}");
+                contador += 2;
+            }
+        }
+    }
+}
