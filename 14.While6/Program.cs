dbb266c8482d9dc83143d7426385ee44e4c45c4f
DannaCaballero <danna.caballero@upb.edu.co>
2025-09-03 09:12:46 -0500

    /Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n.

---
+++ 14.While6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _14.While6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n.*/
+
+            Console.WriteLine("Ingrese un número límite (n): ");
+            int n = int.Parse(Console.ReadLine());
+            int contador = 1;
+            int pares = 0;
+
+            while (contador <= n)
+            {
+                if (contador % 2 == 0)
+                {
+                    pares++;
+                }
+                contador++;
+            }
+            Console.WriteLine("Entre 1 y " + n + " hay " + pares + " números pares ");
+        }
+    }
+}
