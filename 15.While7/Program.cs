1e6719d70103d87d3bff96a786eeb523d6580683
DannaCaballero <danna.caballero@upb.edu.co>
2025-09-03 09:24:59 -0500

    Realizar un algoritmo para determinar cuánto ahorrará una persona en un año, si al final de cada mes deposita cantidades            variables de dinero; además, se quiere saber cuánto lleva ahorrado cada mes.

---
+++ 15.While7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _15.While7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Realizar un algoritmo para determinar cuánto ahorrará una persona en un año, si al final de cada mes deposita cantidades
+           variables de dinero; además, se quiere saber cuánto lleva ahorrado cada mes.*/
+
+            double ahorroTotal = 0;
+
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                Console.Write("Ingresa la cantidad que vas a depositar en el mes " + mes + ": ");
+                double deposito = double.Parse(Console.ReadLine());
+
+                ahorroTotal += deposito;
+
+                Console.WriteLine("Llevas ahorrado hasta el mes " + mes + ": " + ahorroTotal);
+            }
+
+            Console.WriteLine("\nEl ahorro total en el año es: " + ahorroTotal);
+        }
+    }
+}
