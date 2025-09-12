36660ee7385345a04024001aa17e834924b281b2
DannaCaballero <danna.caballero@upb.edu.co>
2025-09-12 09:10:55 -0500

    Programa que genere y sume los 5 primeros numeros enteros.

---
+++ 21.For/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _21.For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que genere y sume los 5 primeros numeros enteros
+
+            int acumulador = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                acumulador += i;
+            }
+            Console.WriteLine($"La suma de los 5 primeros numeros enteros es: {acumulador}");
+
+        }
+    }
+}
