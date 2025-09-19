04b7b82ba734aa19981c6efee46c2e2aaf784c81
DannaCaballero <danna.caballero@upb.edu.co>
2025-09-19 08:03:10 -0500

    //Arreglos Unidimensionales //Vectores

---
+++ 23.Vectores/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _23.Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arreglos Unidimensionales
+            //Vectores
+
+            int[] numeros = new int[5]; //El vector numeros tiene 5 posiciones
+            string[] nombres = new string[4];
+
+            //Asignar valores a las posiciones
+            numeros[0] = 20;
+            numeros[1] = 15;
+            numeros[2] = 5;
+            numeros[3] = 30;
+            numeros[4] = 45;
+            //numeros[5] = 85; Error porque el indice 5 no existe o contradice al vector.
+            int[] numeros1 = new int[] { 5, 12, 4, 7, 9, 21 };
+            int[] numeros2 = { 5, 12, 4, 7, 9 };
+            //Yo puedo recorrer el vector para llenarlo de datos o para recuperar los datos 
+            char[] caracter = new char[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Ingrese el caracter para la posición {i + 1}, con indice {i}:");
+                caracter[i] = Convert.ToChar
+                    (Console.ReadLine());
+            }
+
+            Console.WriteLine($"El vector caracter, en la posición 3, tiene almacenado el dato: {caracter[2]}");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"El vector caracter, en la posición {i + 1}, tiene almacenado el dato: {caracter[i]}");
+            }
+            for (int i = 0; i < caracter.Length; i++) //.Length devuelve el rango del vector
+            {
+                Console.Write(caracter[i] +" | ");
+            }
+
+         //ejercicio 
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine($"Ingrese el nombre para la posición {i + 1}, con indice {i}:");
+                nombres[i] = Convert.ToString(Console.ReadLine());
+            }
+            for (int i = 0; i < nombres.Length; i++)
+            { Console.WriteLine($"El vector en la posición {i + 1}, tiene el nombre:{nombres[i]}"); }
+
+        }
+    }
+}
---
+++ 24.EjercicioArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _24.EjercicioArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*24. Crear arreglo llamado "numeros" de 100 elementos asignar el número 10 en cada una de las posiciones del arreglo. 
+            Leer el contenido de cada elemento y escribirlo en pantalla.*/
+
+            int[] numeros = new int[100];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = 10;
+            }
+
+            Console.WriteLine("Contenido");
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"Posición {i}: {numeros[i]}");
+            }
+        }
+    }
+}
